b13e127ebddb603497f7179ba6f7086b89bf1665
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-23 04:03:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+def dictionary
+  {:hello => 'hi',
+  :to => '2', :two => '2', :too => '2',
+  :for => '4', :four => '4',
+  :be => 'b',
+  :you => 'u',
+  :at => '@',
+  :and => "@"}
+end
+
+def word_substituter(tweet)
+  tweet = tweet.split(" ")
+
+  tweet.each_with_index do |word, index|
+    if dictionary.keys.include?(word)
+      tweet[index] = dictionary[word]
+    end
+  end
+
+  tweet.join(" ")
+
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.size < 140
+    return tweet
+  else
+    return word_substituter(tweet)
+  end
+end
